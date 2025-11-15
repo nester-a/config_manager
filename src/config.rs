7054adc1055cf178c5256065355508ec5103dc6a
nester-a <nester.a.l@yandex.ru>
2025-11-15 13:57:02 +0300

    add fatory method for config creating

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,13 @@
+use std::collections::HashMap;
+
 pub struct AppConfig {
-    database_ulr: String,
+    database_url: String,
     port: u16,
     feature_flags: HashMap<String, bool>,
+}
+
+impl AppConfig{
+    pub fn new(database_url: String, port: u16, feature_flags:HashMap<String, bool>) -> AppConfig{
+        AppConfig { database_url, port, feature_flags }
+    }
 }
