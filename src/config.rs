aa14900cd0664a922a7c061b9b4b1429ab15b9e1
nester-a <nester.a.l@yandex.ru>
2025-11-15 14:01:36 +0300

    add new method for validation control

--- src/config.rs
+++ src/config.rs
@@ -10,4 +10,8 @@ impl AppConfig{
     pub fn new(database_url: String, port: u16, feature_flags:HashMap<String, bool>) -> AppConfig{
         AppConfig { database_url, port, feature_flags }
     }
+
+    pub fn is_ok(&self) -> bool {
+        self.database_url.len() > 0 && self.port > 0 && !self.feature_flags.is_empty()
+    }
 }
